c477b2b6008659d8857d95023c8edc4df454023f
kell-lee <152206637+kell-lee@users.noreply.github.com>
2025-06-17 21:06:51 -0700

    generates one random word

---
+++ src/pages/api/word.js
@@ -0,0 +1,17 @@
+
+
+const baseUrl = 'https://random-word-api.herokuapp.com';
+
+export default async function handler(req, res) {
+    const { count } = req.query.count || 1;
+    const numWords = count ? parseInt(count, 10) : 1;
+
+    try {
+        const response = await fetch(`${baseUrl}/word?number=${numWords}`);
+        const data = await response.json();
+        res.status(200).json({ words: data});
+    } catch (error) {
+        console.error('Error getting words:', error);
+        res.status(500).json({error: 'Failed to get words'});
+    }
+}
